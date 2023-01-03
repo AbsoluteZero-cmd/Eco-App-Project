d277fb067bfdae931be1bb2f05cc7a6b6e4bb20c
Aidana Ualikhan <65579693+AbsoluteZero-cmd@users.noreply.github.com>
2023-01-03 16:48:39 +0600

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:eco_app_project/constants.dart';
 import 'package:eco_app_project/navigation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> _firebaseAuth = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +22,31 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         accentColor: kSecondaryColor
       ),
-      home: Navigation(),
+      home: FutureBuilder(
+        future: _firebaseAuth,
+        builder: (context, snapshot) {
+          if(snapshot.hasError){
+            return Text('Something gone wrong');
+          }
+          else if(snapshot.hasData){
+            FirebaseAuth.instance
+                .userChanges()
+                .listen((User? user) {
+              if (user == null) {
+                print('User is currently signed out!');
+              } else {
+                print('User is signed in!');
+              }
+            });
+            return Navigation();
+          }
+          else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      )
     );
   }
 }
