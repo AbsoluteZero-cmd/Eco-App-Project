355e7bc0c6edb96d2d9589ae7de2f46e0cafaab2
Aidana Ualikhan <65579693+AbsoluteZero-cmd@users.noreply.github.com>
2023-05-31 13:11:13 +0600

    Update archive_detail_page.dart

--- lib/pages/archive_detail_page.dart
+++ lib/pages/archive_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:eco_app_project/constants.dart';
 import 'package:eco_app_project/my_classes.dart';
 import 'package:flutter/material.dart';
@@ -18,10 +19,16 @@ class ArchiveDetailPage extends StatelessWidget {
           children: [
             Hero(
               tag: 'plant${plant.name}',
-              child: Image(
+              // child: Image(
+              //   fit: BoxFit.fitWidth,
+              //   height: 500,
+              //   image: plant.imageURL != '' ? NetworkImage(plant.imageURL) : AssetImage('assets/pine_tree_placeholder.png') as ImageProvider
+              // ),
+              child: CachedNetworkImage(
                 fit: BoxFit.fitWidth,
                 height: 500,
-                image: plant.imageURL != '' ? NetworkImage(plant.imageURL) : AssetImage('assets/pine_tree_placeholder.png') as ImageProvider
+                imageUrl: plant.imageURL,
+                placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
               ),
             ),
             Expanded(
