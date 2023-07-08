bbec513a573666bf24dd3c834bb405f02222f0bb
Aidana Ualikhan <65579693+AbsoluteZero-cmd@users.noreply.github.com>
2023-07-08 13:27:23 +0600

    tried connecting ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import YandexMapsMobile
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    YMKMapKit.setApiKey("254c792f-1f2c-4ac3-8960-b52a769ddc8e")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
